e4c830c889149a6e40a1f9039eb3aa5131437d26
MT <macos@MacBook-Pro.local>
2023-04-17 11:43:36 +0700

    init project

---
+++ ExpenseManagementApp/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ExpenseManagementApp
+//
+//  Created by MT on 14/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var TotalSpendingContainer
+    : UIView!
+    @IBOutlet weak var ExpensesTableOneContainer: UIView!
+    
+    @IBOutlet weak var ExpensesTableTwoContainer: UIView!
+    
+    @IBOutlet weak var NavbarControlUiView: UIView!
+    @IBOutlet weak var ReportNavbarUiView: UIView!
+    @IBOutlet weak var HomeNavbarUiView: UIView!
+    @IBOutlet weak var SettingNavbarUiView: UIView!
+    @IBOutlet weak var CalendarOutlet: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        applyShadowOnview(TotalSpendingContainer)
+        applyShadowOnview(ExpensesTableOneContainer)
+        applyShadowOnview(ExpensesTableTwoContainer)
+        applyShadowOnview(NavbarControlUiView)
+        CalendarOutlet.layer.cornerRadius = 10
+        
+        
+        
+        
+        
+    }
+    func applyShadowOnview(_ view: UIView) {
+        view.layer.cornerRadius = 8
+        view.layer.shadowColor = UIColor.darkGray.cgColor
+        view.layer.shadowOpacity = 0.5
+        view.layer.shadowOffset = .zero
+        view.layer.shadowRadius = 2
+    }
+    
+    
+}
+
---
+++ ReportViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReportViewController.swift
+//  ExpenseManagementApp
+//
+//  Created by MT on 17/04/2023.
+//
+
+import UIKit
+
+class ReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}

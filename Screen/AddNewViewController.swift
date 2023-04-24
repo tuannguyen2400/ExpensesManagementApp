4b3e4769d870d6d460b82d2ff2921d08fd859057
MT <macos@MacBook-Pro.local>
2023-04-24 17:30:14 +0700

    complete logic add new expenses

--- Screen/AddNewViewController.swift
+++ Screen/AddNewViewController.swift
@@ -8,20 +8,24 @@
 import UIKit
 import FloatingPanel
 
-class AddNewViewController: UIViewController,FloatingPanelControllerDelegate {
-
+class AddNewViewController: UIViewController,FloatingPanelControllerDelegate,UITextFieldDelegate {
+    
     @IBOutlet weak var expensesTextField: UITextField!
     @IBOutlet weak var categoryNameTextField: UITextField!
-    
     @IBOutlet weak var moneyExpensesTextField: UITextField!
-    
     @IBOutlet weak var DescriptionTextField: UITextField!
     
+    @IBOutlet weak var categoryView: UIView!
+   
+    
     let fpc = FloatingPanelController()
-
+    var categoryNameText : String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        categoryView.backgroundColor = .clear
+        categoryView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(clickCategoryView)))
+        self.categoryNameTextField.text = categoryNameText
         fpc.delegate = self
         applyShadowOnAddNewview(expensesTextField)
         applyShadowOnAddNewview(categoryNameTextField)
@@ -29,14 +33,21 @@ class AddNewViewController: UIViewController,FloatingPanelControllerDelegate {
         applyShadowOnAddNewview(DescriptionTextField)
     }
     
-
-    @IBAction func showListComponents(_ sender: UIButton) {
+    @objc func clickCategoryView () {
         let contentVC = storyboard?.instantiateViewController(identifier: "CategoryHalfScreen") as? CategoryHalfScreenViewController
         fpc.set(contentViewController: contentVC)
         fpc.addPanel(toParent: self)
     }
-    }
+    //    @IBAction func showListComponents(_ sender: UIButton) {
+    //        let contentVC = storyboard?.instantiateViewController(identifier: "CategoryHalfScreen") as? CategoryHalfScreenViewController
+    //        fpc.set(contentViewController: contentVC)
+    //        fpc.addPanel(toParent: self)
+    //
+    //    }
     
+}
+
+
 
 
 func applyShadowOnAddNewview(_ view: UIView) {
--- Screen/CategoryHalfScreenViewController.swift
+++ Screen/CategoryHalfScreenViewController.swift
@@ -24,12 +24,14 @@ class CategoryHalfScreenViewController: UIViewController , UICollectionViewDataS
         categoriesComponents(imageName: "Restaurant", titleName:"Restaurant"),
         
     ]
+    @IBOutlet weak var addAmountBtnOutlet: UIButton!
     @IBOutlet weak var  categoryCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         categoryCollectionView.register(UICollectionViewCell.self , forCellWithReuseIdentifier: "CategoryCell")
         categoryCollectionView.delegate = self
         categoryCollectionView.dataSource = self
+        applyShadowOnCategoryHalfview(addAmountBtnOutlet)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -46,18 +48,43 @@ class CategoryHalfScreenViewController: UIViewController , UICollectionViewDataS
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let categoryLogoWidth = UIScreen.main.bounds.width
-        let cellWidth = (categoryLogoWidth - 4) / 4
+        let cellWidth = (categoryLogoWidth - 3) / 3
         
         return CGSize(width: cellWidth , height: cellWidth)
     }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 1    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-        
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let namecategory = data[indexPath.row]
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let addnewVc = sb.instantiateViewController(withIdentifier: "AddNewViewController") as! AddNewViewController
+        addnewVc.categoryNameText = namecategory.titleName
+        self.navigationController?.pushViewController(addnewVc, animated: true)
     }
     
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1   }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+        
+        
+    }
+    
+    @IBAction func ClickAddNewDetailsVC(_ sender: Any) {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "AddAmountViewController") as! AddAmountViewController
+        self.navigationController?.pushViewController(vc,
+         animated: true)
+    }
+    
 }
+func applyShadowOnCategoryHalfview(_ view: UIView) {
+    view.layer.borderColor = UIColor.darkGray.withAlphaComponent(0.3).cgColor
+    view.layer.borderWidth = 1
+    view.layer.cornerRadius = 8
+    view.layer.shadowColor = UIColor.darkGray.cgColor
+    view.layer.shadowOffset = .zero
+    view.clipsToBounds = true
+}
+ 
